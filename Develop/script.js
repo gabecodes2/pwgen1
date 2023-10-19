2d2f944420870d24fc1af6a1a2403e65740ef393
gabecodes2 <gabrielcastanheira2002@outlook.com>
2023-10-18 21:06:37 -0400

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,41 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+generateBtn.addEventListener("click", function() {
+  // Prompt for password length
+  var length = prompt("How long would you like your password to be? (8-128 characters)");
 
-  passwordText.value = password;
+  // Validate the input
+  while (length < 8 || length > 128 || isNaN(length)) {
+    alert("Please enter a number between 8-128");
+    length = prompt("How long would you like your password to be? (8-128 characters)");
+  }
 
+  // Prompt for character types
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecialChars = confirm("Include special characters?");
+
+  // Validate character type selection
+  while (!(includeLowercase || includeUppercase || includeNumeric || includeSpecialChars)) {
+    alert("At least one character type should be selected.");
+    includeLowercase = confirm("Include lowercase characters?");
+    includeUppercase = confirm("Include uppercase characters?");
+    includeNumeric = confirm("Include numeric characters?");
+    includeSpecialChars = confirm("Include special characters?");
+  }
+
+  // Generate password based on the validated length and character types
+  var password = generatePassword(length, includeLowercase, includeUppercase, includeNumeric, includeSpecialChars);
+
+// Function to generate password based on given length and character types
+function generatePassword(length, includeLowercase, includeUppercase, includeNumeric, includeSpecialChars) {
+  // Implement your password generation logic here
+  // ...
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Write password to the #password input
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+});
